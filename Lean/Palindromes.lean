56f1ee8111821b3aac533e1ae77aeb4f7a480ff2
Salamun (Sal) Nuhin <52977980+Sal47506@users.noreply.github.com>
2026-01-12 15:55:27 -0500

    Delete Lean directory

--- Lean/Palindromes.lean
+++
@@ -1,8 +0,0 @@
-import Mathlib.Data.List.Basic
-
-namespace Palindromes
-
-def isPalindrome : List α → Prop
-| [] => True
-| [α] => True
-| a → isPalindrome b → isPalindrome (a :: b :: a)
--- Lean/lakefile.lean
+++
@@ -1,7 +0,0 @@
-import Lake
-open Lake DSL
-
-package «Lean» where
-
-require mathlib from git
-  "https://github.com/leanprover-community/mathlib4.git" @ "v4.9.0"
